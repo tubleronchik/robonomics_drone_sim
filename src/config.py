0445d40632666f11f7b85e219481f2c96149b6e9
tubleronchik <m.bystramovich@gmail.com>
2020-09-19 00:47:06 +0300

    first commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+def keys():
+    KEYS = { 
+    'drone': '0xcea615f8afdf10b09cbd47b5c9cb35f661f244c83ef9da729ed35c9524530715', 
+    'not_drone': '0xee50c0baa407778b3be8bb04c1f82921ffaaea13ca8e2af93e28f04b10517f98',
+    'employer': '0x12932533f3b4c6e8599f61090c46cb9c6fad8319d2aae6181ae6c5c625c80691'}
+    return KEYS
+def addresses():
+    ADDRESSES = {'employer': '5Df8mtUb7rfHYRHwH28DpUzx6ZWTPfNwexiaPqqzt3GSBQbu', 
+    'drone': '5GLEUba4eKbkaqxZwdYZmcxS688WEW9UG2rNJDZLDarWGt3G',
+    'not_drone': '5HHLHLyr7Y8uxp2yc6g27qSFRH5uRRSCAopu1gsXNEsRV7ax'}
+    return ADDRESSES
+def dir():
+    DIR = {'DIR': '/home/tubleronchik/'}
+    return DIR['DIR']
+
